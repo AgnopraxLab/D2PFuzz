c53458f3967895687a5567f040531f593132cd44
Hosen <S230130@e.ntu.edu.sg>
2024-12-03 10:47:45 +0800

    fix v4_conncetion

--- fuzzer/v4_maker.go
+++ fuzzer/v4_maker.go
@@ -142,7 +142,7 @@ func (m *V4Maker) PacketStart(traceOutput io.Writer) error {
 	// Iterate over each target object
 	for i := 0; i < 2; i++ {
 		// Print divider line at the start of each iteration
-		logger.Printf("================================================= Starting iteration %d =================================================", i+1)
+		logger.Printf("====================== Starting iteration %d ======================", i+1)
 
 		wg.Add(1)
 
@@ -165,7 +165,7 @@ func (m *V4Maker) PacketStart(traceOutput io.Writer) error {
 		time.Sleep(PacketSleepTime)
 
 		// Print divider line at the end of each iteration
-		logger.Printf("================================================= Completed iteration %d =================================================", i+1)
+		logger.Printf("====================== Completed iteration %d ======================", i+1)
 	}
 
 	wg.Wait()
